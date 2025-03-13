8952e68a495d41311d6bdaffcdd4662524cb4b72
Massimiliano Prandi <maxallep@gmail.com>
2025-03-13 14:15:29 +0100

    aggiunto client con tailwind (sta volta funziona)

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="mt-4 font-extrabold text-4xl text-gray-600 text-center">Selfie</h1>
+  <h2 class="mt-3 text-xl text-gray-400 text-center">forse ce la abbiamo fatta (godo)</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
