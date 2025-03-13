ba8739deedf475b4d4e6e1df3eab5e3637fcc64f
Massimiliano Prandi <maxallep@gmail.com>
2025-03-13 17:13:18 +0100

    layout per home page mobile

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <template>
-  <h1 class="mt-4 font-extrabold text-4xl text-gray-600 text-center">Selfie</h1>
-  <h2 class="mt-3 text-xl text-gray-400 text-center">forse ce la abbiamo fatta (godo)</h2>
+  <div class="min-h-screen flex" style="flex-flow: column;">
+    <nav class="p-4 pt-5 pb-5 mb-7 bg-gray-800 text-gray-200 text-3xl">
+      Logo
+    </nav>
+
+    <div class="flex-auto bg-blue-600 ml-5 mr-5 mb-4 text-center">Calendario</div>
+    
+  
+    <div class="grid grid-cols-2  grid-rows-1 gap-4 text-center">
+      <div class="ml-5 mb-7 aspect-square bg-yellow-400">Note</div>
+      <div class="mr-5 mb-7 aspect-square bg-red-600">Pomodoro</div>
+    </div>
+
+  </div>
 </template>
 
 <script>
