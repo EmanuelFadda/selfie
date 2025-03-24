816c65294987b40a30e9cb73430950a91acc63c7
Massimiliano Prandi <maxallep@gmail.com>
2025-03-24 23:01:28 +0100

    aggiunto dark mode al login e standardizzato colori come per le altre view

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,40 +1,38 @@
 <template>
-  <section class="lex min-h-screen bg-gray-50">
-    <div class="mb-16 flex w-screen flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-      <h1 class="mb-10 text-center text-3xl font-semibold text-gray-900 md:text-4xl">Selfie</h1>
-      <div class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
-        <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-          <h1 class="text-xl font-bold tracking-tight text-gray-900 md:text-2xl">Accedi al tuo account</h1>
-          <form class="space-y-4 md:space-y-6" action="#">
-            <div>
-              <label for="email" class="mb-2 block text-sm font-medium text-gray-900">Email</label>
-              <input type="email" name="email" id="email" class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900" placeholder="email@unibo.it" required="" />
-            </div>
-            <div>
-              <label for="password" class="mb-2 block text-sm font-medium text-gray-900">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••" class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900" required="" />
-            </div>
-            <div class="flex items-center justify-between">
-              <div class="flex items-start">
-                <div class="flex h-5 items-center">
-                  <input id="remember" aria-describedby="remember" type="checkbox" class="h-4 w-4 rounded border border-gray-300" required="" />
-                </div>
-                <div class="ml-2 text-sm">
-                  <label for="remember" class="text-gray-400">Ricordami</label>
-                </div>
+  <div class="mb-16 flex min-h-screen w-screen flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+    <h1 class="mb-10 text-center text-3xl font-bold md:text-4xl">Selfie</h1>
+    <div class="w-full rounded-lg sm:max-w-md md:mt-0 xl:p-0">
+      <div class="p-6 sm:p-8">
+        <h1 class="mb-4 text-xl font-bold tracking-tight md:text-2xl">Accedi al tuo account</h1>
+        <form class="" action="#">
+          <div>
+            <label for="email" class="mb-2 block text-sm font-medium">Email</label>
+            <input type="email" name="email" id="email" class="mb-4 block w-full rounded-lg p-2.5 focus:outline focus:outline-neutral-800 md:mb-6 dark:bg-neutral-800 dark:focus:outline-slate-50" placeholder="email@unibo.it" required="" />
+          </div>
+          <div>
+            <label for="password" class="mb-2 block text-sm font-medium">Password</label>
+            <input type="password" name="password" id="password" placeholder="••••••••" class="mb-4 block w-full rounded-lg p-2.5 focus:outline focus:outline-neutral-800 md:mb-6 dark:bg-neutral-800 dark:focus:outline-slate-50" required="" />
+          </div>
+          <div class="flex items-center justify-between">
+            <div class="flex items-start">
+              <div class="flex h-5 items-center">
+                <input id="remember" aria-describedby="remember" type="checkbox" class="h-4 w-4 accent-neutral-800" required="" />
               </div>
-              <a href="#" class="text-sm font-medium text-gray-600 hover:underline">Password dimenticata?</a>
+              <div class="ml-2 text-sm">
+                <label for="remember" class="">Ricordami</label>
+              </div>
             </div>
-            <button type="submit" class="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4">Sign in</button>
-            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-              Non hai ancora un account?
-              <a href="#" class="text-primary-600 font-medium hover:underline">Registrati</a>
-            </p>
-          </form>
-        </div>
+            <a href="#" class="text-sm font-medium hover:underline">Password dimenticata?</a>
+          </div>
+          <button type="submit" class="mb-4 mt-10 w-full rounded-lg bg-white px-5 py-2.5 text-center text-sm font-medium focus:outline md:mb-6 md:mt-12 dark:bg-neutral-800">Sign in</button>
+          <p class="text-sm font-light">
+            Non hai ancora un account?
+            <a href="#" class="font-medium hover:underline">Registrati</a>
+          </p>
+        </form>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
