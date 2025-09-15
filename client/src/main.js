5bb6dfb9ffe6dc576a9ad3d33dfb6b2adfbece20
EmanuelFadda <fadda.dev@gmail.com>
2025-09-15 21:28:17 +0200

    importata la libreria per il calendario

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,19 @@ import { createPinia } from "pinia"
 import router from "./router"
 import "./assets/tailwind.css"
 
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/labs/VCalendar'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  directives,
+  components
+})
+
 createApp(App)
   .use(createPinia())
   .use(router)
+  .use(vuetify)
   .mount("#app")
